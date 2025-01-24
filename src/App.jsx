26aee6463e4019f97ddd71e49b10b5fdbaf53775
Vladislav Ropot <vladislav.ropot.dev@gmail.com>
2025-01-24 17:22:38 +0200

    add new functional, saveInput and edit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 
 function App() {
+  const defaultTaskValue = {
+    id: null,
+    name: '',
+    completed: false,
+  }
+  const [inputValue, setInputValue] = useState(defaultTaskValue);
+  const [tasks, setTasks] = useState([]);
+
+  const handleSave = () => {
+    if (inputValue.id === null) {
+      inputValue.id = tasks.length + 1;
+
+      setTasks([...tasks, inputValue]);
+    } else {
+      const index = tasks.findIndex(item => item.id === inputValue.id);
+        if (index !== -1) {
+          tasks[index] = inputValue;
+        }
+
+        setTasks([...tasks]);
+    }
+
+    setInputValue(defaultTaskValue);
+  };
   return (
     <div >
       <div>
-        <input type="text" placeholder="Add a new task" />
-        <button>Save</button>
+        <input type="text"
+          placeholder="Add a new task"
+          value={inputValue.name}
+          onInput={(event) => setInputValue({...inputValue, name: event.target.value})} />
+        <button onClick={() => handleSave()}>Save</button>
       </div>
 
       <div>
@@ -17,23 +45,18 @@ function App() {
       </div>
       <div>
         <ul>
-          <li>
-            <input type="checkbox" />
-            <span>Task name</span>
-            <div>
-              <button>Edit</button>
-              <button>Remove</button>
-            </div>
-          </li>
-
-          <li>
-            <input type="checkbox" />
-            <span>Task name</span>
-            <div>
-              <button>Edit</button>
-              <button>Remove</button>
-            </div>
-          </li>
+          {tasks.map(task => (
+            <li key={task.id}>
+              <input type="checkbox"
+                checked={task.completed} />
+              <span>{task.name}</span>
+              <div>
+                <button onClick={() => setInputValue(task)}>Edit</button>
+                <button>Remove</button>
+              </div>
+            </li>
+          ))}
+          
         </ul>
       </div>
     </div>
